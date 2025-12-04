9b4bb417c79d820c256ba9be2c27ed206d49db0d
Jesse Gibson <JesseTheGibson@gmail.com>
2025-12-04 18:03:51 -0500

    Add CLI and Tree-sitter grammar to flake packages
    - ambient: CLI binary built with Rust toolchain from rust-toolchain.toml
    - tree-sitter-ambient: Generated grammar with parser.c and queries
    - default package points to ambient CLI
    - Fix deprecated devShell to devShells.default

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Development environment";
+  description = "Ambient programming language";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -31,15 +31,68 @@
     in
 
     {
-      devShell = eachSystem (
+      packages = eachSystem (
         system: pkgs:
-        pkgs.mkShell {
-          packages = [
-            (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
-            pkgs.just
-            pkgs.tree-sitter
-            pkgs.nodejs
-          ];
+        let
+          rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = rustToolchain;
+            rustc = rustToolchain;
+          };
+        in
+        {
+          ambient = rustPlatform.buildRustPackage {
+            pname = "ambient";
+            version = "0.1.0";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+            meta = {
+              description = "Ambient programming language CLI";
+              mainProgram = "ambient";
+            };
+          };
+
+          tree-sitter-ambient = pkgs.stdenv.mkDerivation {
+            pname = "tree-sitter-ambient";
+            version = "0.1.0";
+            src = ./tree-sitter-ambient;
+
+            nativeBuildInputs = [
+              pkgs.tree-sitter
+              pkgs.nodejs
+            ];
+
+            buildPhase = ''
+              tree-sitter generate
+            '';
+
+            installPhase = ''
+              mkdir -p $out
+              cp -r src $out/
+              cp -r queries $out/
+              cp grammar.js $out/
+              cp package.json $out/
+            '';
+
+            meta = {
+              description = "Tree-sitter grammar for the Ambient programming language";
+            };
+          };
+
+          default = self.packages.${system}.ambient;
+        }
+      );
+
+      devShells = eachSystem (
+        system: pkgs: {
+          default = pkgs.mkShell {
+            packages = [
+              (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+              pkgs.just
+              pkgs.tree-sitter
+              pkgs.nodejs
+            ];
+          };
         }
       );
     };
